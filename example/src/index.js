3ba97e12dbf6cf37173c6e2a79f418ca7f9fec2e
Kim Brandwijk <kim.brandwijk@gmail.com>
2018-01-12 14:16:01 +0100

    feat(binding): updated to graphql-binding v1 (deps, examples, readme)

--- example/src/index.js
+++ example/src/index.js
@@ -15,7 +15,7 @@ const resolvers = {
     hello: (parent, { name }) => `Hello ${name || 'World'}!`,
     favoriteRepos: (parent, args, context, info) => {
       return Promise.all(
-        favoriteRepos.map(args => github.query.repository(args, info)),
+        favoriteRepos.map(args => github.query.repository(args, context, info)),
       )
     },
   },
