31eaa872ab76e4dec54576aa7c7f361280ea3a91
timsuchanek <tim.suchanek@gmail.com>
2018-05-04 17:23:58 +0200

    fix typings, add codegen to build script

--- src/generated-binding.ts
+++ src/generated-binding.ts
@@ -1,5 +1,6 @@
-import { makeBindingClass } from 'graphql-binding'
-import { GraphQLResolveInfo } from 'graphql'
+import { makeBindingClass, Options } from 'graphql-binding'
+import { GraphQLResolveInfo, GraphQLSchema } from 'graphql'
+import { IResolvers } from 'graphql-tools/dist/Interfaces'
 import schema from  './schema'
 
 export interface Query {
@@ -66,6 +67,13 @@ export interface BindingInstance {
   mutation: Mutation
   subscription: Subscription
   request: <T = any>(query: string, variables?: {[key: string]: any}) => Promise<T>
+  delegate(operation: 'query' | 'mutation', fieldName: string, args: {
+      [key: string]: any;
+  }, infoOrQuery?: GraphQLResolveInfo | string, options?: Options): Promise<any>;
+  delegateSubscription(fieldName: string, args?: {
+      [key: string]: any;
+  }, infoOrQuery?: GraphQLResolveInfo | string, options?: Options): Promise<AsyncIterator<any>>;
+  getAbstractResolvers(filterSchema?: GraphQLSchema | string): IResolvers;
 }
 
 export interface BindingConstructor<T> {
